28a2393c6c332befe59e0aa882c8790f375194b2
Stacey Sugiono <staceysugiono@gmail.com>
2015-11-30 17:12:05 -0800

    get player names, added background

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function newGame(){
   window.location.reload();
 }
 
-
+//Creates both players' grid
 function createGrid(board){
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  var tbl = document.createElement('table');
+  var tblBody = document.createElement('tbody');
 
   for(var i = 0; i < 10; i++){
-    var row = document.createElement("tr");
+    var row = document.createElement('tr');
     for(var j = 0; j < 10; j++){
-      var col = document.createElement("td");
+      var col = document.createElement('td');
       row.appendChild(col);
     }
     tblBody.appendChild(row);
@@ -18,5 +18,23 @@ function createGrid(board){
   tbl.appendChild(tblBody);
   board.appendChild(tbl);
 }
-createGrid(document.getElementById("board1"));
-createGrid(document.getElementById("board2"))
+createGrid(document.getElementById('board1'));
+createGrid(document.getElementById('board2'))
+
+
+//Get Player Names
+function getPlayerName(player){
+  var name = prompt("Please enter your player name: ");
+  if (name){
+    document.getElementById(player).innerHTML = "Player: " + name;
+  } else {
+    getPlayerName(player);
+  }
+}
+getPlayerName('name1');
+getPlayerName('name2');
+
+
+
+
+
